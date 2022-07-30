2882538f716deff328ed94d6a0270c3dcb7ad2a9
Amine MEBARKI <medaminemebarki@gmail.com>
2022-07-30 19:55:28 +0200

    splitting components into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 function ExpenseItem(props) {
 const date = props.date;
 const title = props.title;
 const price = props.amount;
+
     return (
         <div className='expense-item'>
-            <div>{date.toISOString()}</div>
+          <ExpenseDate date={date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${price}</div>
