1b599f13ea3c34f3e03e79f2e550852283111716
Amine MEBARKI <medaminemebarki@gmail.com>
2022-07-30 12:36:57 +0200

    outputting dynamic data nd working with expressions in jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import './ExpenseItem.css'
 function ExpenseItem() {
+const date = new Date();
+const title = "Car Insurance";
+const price = 294.64;
     return (
         <div className='expense-item'>
-            <div>March 24th 2022</div>
+            <div>{date.getDate()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$294.64</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${price}</div>
             </div>
         </div>
     )
