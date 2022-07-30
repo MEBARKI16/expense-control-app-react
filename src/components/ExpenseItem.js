1cb6047d17d7c3d55aeaffed049de8f12a1dc4ca
Amine MEBARKI <medaminemebarki@gmail.com>
2022-07-30 23:40:16 +0200

    using concept of Composition (children props)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+import Card from '../Card';
 function ExpenseItem(props) {
 const date = props.date;
 const title = props.title;
 const price = props.amount;
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
           <ExpenseDate date={date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
                 <div className='expense-item__price'>${price}</div>
             </div>
-        </div>
+        </Card>
     )
 };
 export default ExpenseItem;
