24217381c69b7b4a2bdfa48df231cb65df9739e3
Amine MEBARKI <medaminemebarki@gmail.com>
2022-08-04 13:22:16 +0200

    Outputting Conditional Content

--- src/Expense.js
+++ src/Expense.js
@@ -3,9 +3,12 @@ import './Expense.css'
 import Card from './Card'
 function Expense(props){
    const expenses = props.expense;
+    let x;
+   if (expenses.length === 0) { x = <p>No Expenses Found.</p>}
+   else { x = expenses.map(x => <ExpenseItem key={x.id} title={x.title} amount={x.amount} date={x.date}/>)}
     return(
         <Card className="expenses">
-        {expenses.map(x => <ExpenseItem key={x.id} title={x.title} amount={x.amount} date={x.date}/>)}
+            {x}       
         </Card>
     )
 }
